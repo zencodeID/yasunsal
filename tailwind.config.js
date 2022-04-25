01a616cd31815a135a4db06dd0f7c9c00af3e70d
Moh Subhan <subkh4n@gmail.com>
2022-04-25 14:20:16 +0700

    finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        utama: "#E5E3C9",
+        kedua: "#B4CFB0",
+        ketiga: "#94B49F",
+        dark: "#525E75",
+      },
+    },
   },
   plugins: [],
 };
